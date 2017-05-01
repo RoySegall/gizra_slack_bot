11c6da46819a647b87168f22e7cb2bf70d1262fb
Roy Segall <roy.segall@gizra.com>
2017-05-01 17:50:36 +0300

    Fixing tests?

--- src/Examples/GitHubOpened/NuntiusGitHubOpenedExample.php
+++ src/Examples/GitHubOpened/NuntiusGitHubOpenedExample.php
@@ -15,20 +15,10 @@ class NuntiusGitHubOpenedExample implements GitHubOpenedEventInterface {
    * {@inheritdoc}
    */
   public function act(GitHubEvent $event) {
-    // Get the data from the event.
-    $data = $event->getData();
-
-    $key = empty($data->pull_request) ? 'issue' : 'pull_request';
-
     // Build the info variable to the post message method.
-    $info = [
-      'image' => $data->{$key}->user->avatar_url,
-      'username' => $data->{$key}->user->login,
-      'url' => $data->{$key}->html_url,
-      'title' => $data->{$key}->title,
-      'body' => $data->{$key}->body,
-      'created' => $data->{$key}->created_at,
-    ];
+    $info = $this->getPayload($event);
+
+    $key = $info['key'];
 
     $info['text'] = 'Hi ' . $info['username'];
     $info['text'] .= $key == 'issue' ? ', You created an issue' : ', You created a PR';
@@ -36,6 +26,32 @@ class NuntiusGitHubOpenedExample implements GitHubOpenedEventInterface {
     $info['footer'] = 'Created at ' . $info['created'];
 
     $this->postMessage($info);
+  }
+
+  /**
+   * Process the event into a payload.
+   *
+   * @param GitHubEvent $event
+   *   Object with GitHub event.
+   *
+   * @return array
+   *   The payload.
+   */
+  protected function getPayload(GitHubEvent $event) {
+    $data = $event->getData();
+
+    $key = empty($data->pull_request) ? 'issue' : 'pull_request';
+
+    // Build the info variable to the post message method.
+    return [
+      'image' => $data->{$key}->user->avatar_url,
+      'username' => $data->{$key}->user->login,
+      'url' => $data->{$key}->html_url,
+      'title' => $data->{$key}->title,
+      'body' => $data->{$key}->body,
+      'created' => $data->{$key}->created_at,
+      'key' => $key,
+    ];
   }
 
   /**
