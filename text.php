8e0cbe122d5d20bd472c7498a1d7e6c7ed22688a
Roy Segall <roy@segall.io>
2018-04-22 19:06:11 +0300

    Now?

--- text.php
+++ text.php
@@ -4,4 +4,9 @@ require_once 'vendor/autoload.php';
 
 $db = \Nuntius\Nuntius::getDb();
 
-$db->getOperations()->dbDrop('foo');
+//$db->getOperations()->tableCreate('users');
+//\Kint::dump($db->getOperations()->tableExists('users'));
+//\Kint::dump($db->getOperations()->tableExists('noy'));
+$db->getOperations()->indexCreate('noy', ['name' => 13]);
+$db->getOperations()->indexDrop('noy', 'name_13');
+\Kint::dump($db->getOperations()->indexExists('noy', 'name_1'));
