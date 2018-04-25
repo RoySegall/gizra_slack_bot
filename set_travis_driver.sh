0bf9c788f1af3d6cfa9f72d731157fa042464909
Roy Segall <roy@segall.io>
2018-04-25 13:53:20 +0300

    Now?

--- set_travis_driver.sh
+++ set_travis_driver.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-echo "db_driver: '$1'" >> settings/credentials.local.travis.yml
+echo db_driver: "$1" >> settings/credentials.local.travis.yml
 cat settings/credentials.local.travis.yml
