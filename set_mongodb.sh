629e799bafa601856dc13c934c3f66a353585643
Roy Segall <roy@segall.io>
2018-05-12 20:41:44 +0300

    Order some stuff

--- set_mongodb.sh
+++ set_mongodb.sh
@@ -2,7 +2,10 @@
 
 # Install MongoDB for PHP.
 # source: https://gist.github.com/gyndav/2351174.
-wget http://pecl.php.net/get/mongodb-1.4.3.tgz
-tar -xzf mongodb-1.4.3.tgz
-sh -c "cd mongodb-1.4.3 && pecl install mongodb"
-echo "extension=mongodb.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+if [ $1 == "mongodb" ]
+then
+  wget http://pecl.php.net/get/mongodb-1.4.3.tgz
+  tar -xzf mongodb-1.4.3.tgz
+  sh -c "cd mongodb-1.4.3 && pecl install mongodb"
+  echo "extension=mongodb.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+fi
