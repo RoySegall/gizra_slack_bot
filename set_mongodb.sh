b9f5293cb348747f5056cbc1b8afd38fc490ab94
Roy Segall <roy@segall.io>
2018-05-12 20:43:52 +0300

    Fixing mongo.

--- set_mongodb.sh
+++ set_mongodb.sh
@@ -2,10 +2,7 @@
 
 # Install MongoDB for PHP.
 # source: https://gist.github.com/gyndav/2351174.
-if [ $1 == "mongodb" ]
-then
-  wget http://pecl.php.net/get/mongodb-1.4.3.tgz
-  tar -xzf mongodb-1.4.3.tgz
-  sh -c "cd mongodb-1.4.3 && pecl install mongodb"
-  echo "extension=mongodb.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
-fi
+wget http://pecl.php.net/get/mongodb-1.4.3.tgz
+tar -xzf mongodb-1.4.3.tgz
+sh -c "cd mongodb-1.4.3 && pecl install mongodb"
+echo "extension=mongodb.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
